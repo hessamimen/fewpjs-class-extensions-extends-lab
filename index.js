3d0b99e93fd03798727c3e07f6d6705a7e1a2f2f
Hessam Imen <hessam.imen@gmail.com>
2022-04-03 14:00:25 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-// Your code here
+// // Your code here
+class Polygon{
+    constructor(integersArr){
+        this.shapeSide = integersArr
+        }
+    get countSides(){
+        return this.shapeSide.length;
+    }
+    get perimeter(){
+        return this.shapeSide.reduce((previousValue, currentValue) =>  previousValue + currentValue)
+    }
+}
+        
+class Triangle extends Polygon{
+            get isValid(){
+        if(this.countSides !== 3) return
+        let side1 = this.shapeSide[0];
+        let side2 = this.shapeSide[1];
+        let side3 = this.shapeSide[2];
+        return (
+            (side1 + side2 > side3) && (side2+side3>side1) && (side1+side3>side2)
+        )
+
+            }
+        }
+
+class Square extends Polygon{
+    get isValid(){
+        if(this.countSides !== 4) return
+        let side1 = this.shapeSide[0];
+        let side2 = this.shapeSide[1];
+        let side3 = this.shapeSide[2];
+        let side4 = this.shapeSide[3];
+    return (side1 === side2 && side1 === side3 && side1 === side4)
+    }
+    get area(){
+        return this.shapeSide[0] * this.shapeSide[1];
+    }
+}
+
+let square = new Square( [ 5, 5, 5, 5 ] )
+let square2 = new Square( [ 5, 4, 3 ,2] )
+
+console.log(square2.isValid)
+// square2.isValid
